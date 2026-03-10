487b5813e02d720a801f6dda855352b6a1fe83bb
sanya132007 <a58812007@gmail.com>
2026-03-10 16:18:36 +0000

    ainda vai sofrer alterações

--- PROCESSOS/process_carrinho_remover.php
+++ PROCESSOS/process_carrinho_remover.php
@@ -1,29 +1,16 @@
 <?php
-
 session_start();
 
-include '../BASE_DE_DADOS/ligacao_bd.php';
+if(isset($_GET['id'])){
+    $id = (int)$_GET['id'];
 
-
-
-if (isset($_GET['id']) && isset($_SESSION['id_utilizador'])) {
-
-    $id_carrinho = $_GET['id'];
-
-    $id_user = $_SESSION['id_utilizador'];
-
-
-
-    $sql = "DELETE FROM carrinho WHERE id_carrinho = ? AND id_utilizador = ?";
-
-    $stmt = $pdo->prepare($sql);
-
-    $stmt->execute([$id_carrinho, $id_user]);
-
+    if(isset($_SESSION['carrinho'])){
+        $_SESSION['carrinho'] = array_filter(
+            $_SESSION['carrinho'],
+            fn($pid) => $pid !== $id
+        );
+    }
 }
 
-
-
+header("Location: ../carrinho.php");
 exit;
-
-?>
