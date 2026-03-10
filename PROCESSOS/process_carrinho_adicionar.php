00b031f8f4b7a9f03e940d689147fd090e1865c9
sanya132007 <a58812007@gmail.com>
2026-03-10 15:53:51 +0000

    simplificar ao maximo

--- PROCESSOS/process_carrinho_adicionar.php
+++ PROCESSOS/process_carrinho_adicionar.php
@@ -1,75 +1,17 @@
 <?php
-
 session_start();
 
-include '../BASE_DE_DADOS/ligacao_bd.php';
+if(isset($_POST['produto_id'])){
+    $produto_id = (int)$_POST['produto_id'];
 
-
-
-if (!isset($_SESSION['id_utilizador'])) {
-
-    echo json_encode(['status' => 'erro', 'mensagem' => 'Login obrigatório!']);
-
-    exit;
-
-}
-
-
-
-if (isset($_POST['id_produto'])) {
-
-    $id_produto = $_POST['id_produto'];
-
-    $id_utilizador = $_SESSION['id_utilizador'];
-
-
-
-    try {
-
-        $check = $pdo->prepare("SELECT id_carrinho FROM carrinho WHERE id_utilizador = ? AND id_produto = ?");
-
-        $check->execute([$id_utilizador, $id_produto]);
-
-       
-
-        if ($check->rowCount() > 0) {
-
-            echo json_encode(['status' => 'sucesso', 'mensagem' => 'O produto já está no seu carrinho!']);
-
-            exit;
-
-        }
-
-
-
-        $sql = "INSERT INTO carrinho (id_utilizador, id_produto) VALUES (?, ?)";
-
-        $stmt = $pdo->prepare($sql);
-
-       
-
-        if ($stmt->execute([$id_utilizador, $id_produto])) {
-
-            echo json_encode(['status' => 'sucesso', 'mensagem' => 'Produto adicionado ao seu carrinho!']);
-
-        } else {
-
-            echo json_encode(['status' => 'erro', 'mensagem' => 'Falha ao adicionar o produto.']);
-
-        }
-
-
-
-    } catch (PDOException $e) {
-
-        echo json_encode(['status' => 'erro', 'mensagem' => 'Erro na base de dados: ' . $e->getMessage()]);
-
+    if(!isset($_SESSION['carrinho'])){
+        $_SESSION['carrinho'] = [];
     }
 
-} else {
-
-    echo json_encode(['status' => 'erro', 'mensagem' => 'Dados inválidos.']);
-
+    if(!in_array($produto_id, $_SESSION['carrinho'])){
+        $_SESSION['carrinho'][] = $produto_id;
+    }
 }
 
-?>
+header("Location: ../carrinho.php");
+exit;
