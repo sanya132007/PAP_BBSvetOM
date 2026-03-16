263379976471fd38f6f34ebb9a5b6cb2edc7a7e9
sanya132007 <a58812007@gmail.com>
2026-03-16 16:11:01 +0000

    corrigi o erro, agora apaga produto

--- PROCESSOS/process_carrinho_remover.php
+++ PROCESSOS/process_carrinho_remover.php
@@ -9,6 +9,46 @@ if(!isset($_SESSION['cliente_id']) || !isset($_GET['id'])) {
 $cliente_id = (int)$_SESSION['cliente_id'];
 $id_produto = (int)$_GET['id'];
 
-$stmt = $pdo->prepare("DELETE FROM carrinho WHERE id_cliente = :cliente AND id_produto = :produto");
-$stmt->execute(['cliente' => $cliente_id, 'produto' => $id_produto]);
+$stmt = $pdo->prepare("
+    SELECT quantidade 
+    FROM carrinho 
+    WHERE id_cliente = :cliente AND id_produto = :produto
+");
+
+$stmt->execute([
+    'cliente' => $cliente_id,
+    'produto' => $id_produto
+]);
+
+
+$item = $stmt->fetch(PDO::FETCH_ASSOC);
+
+if(!$item){
+    exit;
+}
+
+if($item['quantidade'] > 1){
+
+    /* diminui quantidade */
+    $stmt = $pdo->prepare("
+        UPDATE carrinho 
+        SET quantidade = quantidade - 1 
+        WHERE id_cliente = :cliente AND id_produto = :produto
+    ");
+
+} else {
+
+    /* remove produto */
+    $stmt = $pdo->prepare("
+        DELETE FROM carrinho 
+        WHERE id_cliente = :cliente AND id_produto = :produto
+    ");
+
+}
+
+$stmt->execute([
+    'cliente' => $cliente_id,
+    'produto' => $id_produto
+]);
+
 exit;
